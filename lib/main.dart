05866060d170b4c2ec1bc65afc3eea2ad717a8f8
Akila Prabath <akilaprabath861@gmail.com>
2026-06-14 20:33:18 +0530

    Initial Flutter project setup

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Weather App V2'),
+        ),
+        body: const Center(
+          child: Text(
+            'Weather App Setup Complete',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
